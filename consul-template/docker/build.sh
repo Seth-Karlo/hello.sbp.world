0659305957185d7a82400563f96e388b14cd4c55
Andrew Repton <arepton@schubergphilis.com>
2017-02-22 17:42:26 +0100

    Adding build stuff for consul-template

---
+++ consul-template/docker/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Borrowed from https://github.com/skippy/docker-repo/blob/master/consul-template/build.sh
+
+set -m
+
+mkdir ${1}
+cd ${1}
+git clone https://github.com/hashicorp/consul-template
+cd consul-template
+git checkout ${1}
+
+export XC_OS="linux"
+export XC_ARCH="amd64"
+
+make bin
+
+cd ../..
+mv ${1}/consul-template/pkg/linux_amd64/consul-template .
+
+docker build -t registry.services.schubergphilis.com:5000/saas/consul-template:${1} .
+
+docker push registry.services.schubergphilis.com:5000/saas/consul-template:${1}
